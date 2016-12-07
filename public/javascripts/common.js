49b50c6dad0799d00c1c1b9b88d51bfc9fc7aa72
Alexander Li <alexander.li@ef.com>
2016-12-07 16:42:56 +0800

    polish date and make adjustments on pages

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,3 +1,19 @@
 $(function (){
-
+    var index = location.href.indexOf(location.host),
+        len = location.host.length,
+        name = location.href.slice(index + len + 1);
+    if (name[name.length - 1] === '/') {
+        name = name.slice(0, -1);
+    }
+    if (name === '') {
+        name = 'index';
+    }
+    $('.nav>li>a').each(function (i){
+        if ($(this).attr('name') == name) {
+            $(this).addClass('active');
+        }
+        else {
+            $(this).removeClass('active');
+        }
+    });
 });
--- routes/history.js
+++ routes/history.js
@@ -3,12 +3,13 @@
 const
     express = require('express'),
     router = express.Router(),
-    conn = require('../libs/pool');
+    conn = require('../libs/pool'),
+    dateFormat = require('dateformat');
 
-router.get('/:userName', function (req, res){
+router.get('/', function (req, res){
     let
         params = req.params,
-        sql = "select * from application where renter = '" + params.userName + "'";
+        sql = "select * from application ";
 
     conn.getConnection(function (err, connection) {
         conn.query(sql, (err, rows, fields) => {
@@ -16,6 +17,12 @@ router.get('/:userName', function (req, res){
                 console.log(err.stack);
                 return;
             }
+            rows.forEach(function (row){
+                let formatStr = 'yyyy-mm-dd';
+                row.createTime = dateFormat(row.createTime, formatStr);
+                row.startTime = dateFormat(row.startTime, formatStr);
+                row.endTime = dateFormat(row.endTime, formatStr);
+            });
             res.render('history', {
                 title: '租车纪录',
                 recs: rows 
